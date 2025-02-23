8f92bf604e20d40e5d309a7b04ceea9744518b54
JhonJSC182 <jhonjsc182@hotmail.com>
2025-02-23 13:10:05 -0800

    started backend

--- backend/starting_folder/server.js
+++ backend/starting_folder/server.js
@@ -3,5 +3,16 @@ const app = express()
 const PORT = 8383
 
 
+// HTTP VERBS && ROUTES (or path)
+
+app.get('/', (req, res) => {
+    console.log('Yay I hit an end point', req.method)
+    res.sendStatus(201)
+})
+
+app.get('/dashboard', (req, res) => {
+    console.log('Ohhh now I hit the /dashboard endpoint')
+    res.send('hi')
+})
 
 app.listen(PORT, () => console.log(`Server has started on : ${PORT}`))
