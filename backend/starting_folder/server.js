347c6903003e36d8e9bee9e5cdc4cdbbf7a681fd
JhonJSC182 <jhonjsc182@hotmail.com>
2025-02-25 18:13:07 -0800

    up to date

--- backend/starting_folder/server.js
+++ backend/starting_folder/server.js
@@ -3,7 +3,14 @@ const app = express()
 const PORT = 8383
 
 
+let data = {
+    name: 'james'
+}
+
+
 // HTTP VERBS && ROUTES (or path)
+
+// Type 1 - Website endpoints (sends back html)
 
 app.get('/', (req, res) => {
     console.log('Yay I hit an end point', req.method)
@@ -14,5 +21,13 @@ app.get('/dashboard', (req, res) => {
     console.log('Ohhh now I hit the /dashboard endpoint')
     res.send('hi')
 })
+
+// Type 2 - API endpoints (non visual
+
+app.get('/api/data', (req, res) => {
+    console.log('This one was for   data')
+    res.send(data)
+})
+
 
 app.listen(PORT, () => console.log(`Server has started on : ${PORT}`))
